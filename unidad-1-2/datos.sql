e9a9405dd4a2c322605a1e4ae5adb795aba33c03
tomasmalio <tomasmalio@gmail.com>
2017-09-05 00:07:02 -0300

    Subiendo Trabajo Práctivo 2

--- unidad-1-2/datos.sql
+++ unidad-1-2/datos.sql
@@ -1,4 +1,35 @@
 /**
  *	DATOS
  **/
- 
+INSERT INTO Especialidad (id, nombre_especialidad)
+VALUES (1, 'Alergología'),
+(2, 'Anestesiología y reanimación'),
+(3, 'Cardiología'),
+(4, 'Gastroenterología'),
+(5, 'Endocrinología'),
+(6, 'Geriatría'),
+(7, 'Hematología y hemoterapia'),
+(8, 'Infectología'),
+(9, 'Medicina aeroespacial'),
+(10, 'Medicina del deporte'),
+(11, 'Medicina del trabajo'),
+(12, 'Medicina de urgencias'),
+(13, 'Medicina familiar y comunitaria'),
+(14, 'Medicina física y rehabilitación'),
+(15, 'Medicina intensiva'),
+(16, 'Medicina interna'),
+(17, 'Medicina legal y forense'),
+(18, 'Medicina preventiva y salud pública'),
+(19, 'Nefrología'),
+(20, 'Neumología'),
+(21, 'Neurología'),
+(22, 'Nutriología'),
+(23, 'Oftalmología'),
+(24, 'Oncología médica'),
+(25, 'Oncología radioterápica'),
+(26, 'Pediatría'),
+(27, 'Psiquiatría'),
+(28, 'Rehabilitación'),
+(29, 'Reumatología'),
+(30, 'Toxicología'),
+(30, 'Urología');
