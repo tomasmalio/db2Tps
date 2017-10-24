46466554f9355e1221aed5892b175d08016b8d72
tomasmalio <tomasmalio@gmail.com>
2017-10-24 14:20:50 -0300

    Update parcial_23-05-2017.sql
    Update of the ej 1

--- modelos-de-parcial/parcial_23-05-2017.sql
+++ modelos-de-parcial/parcial_23-05-2017.sql
@@ -4,21 +4,24 @@
 		Retome la diferencia de edad en dias entre el jugador más joven de la categoria 85 y el más viejo de la categoria 84, de cada club.
 		La consulta debe ser correlacionada
 **/
+CREATE VIEW vw1037546_23052017
+AS
+	SELECT datediff(dd, jViejo.Fecha_Nac, jJoven.Fecha_Nac) AS 'Diferencia De Dias', c.Nombre, c.Id_Club
+	FROM Clubes c,
+		(
+			SELECT MIN(j4.Fecha_Nac) Fecha_Nac, j4.Id_Club
+			FROM Jugadores j4
+			WHERE j4.Categoria = 84
+			GROUP BY j4.Id_Club
+		) jViejo,
+		(
+			SELECT MAX(j5.Fecha_Nac) Fecha_Nac, j5.Id_Club
+			FROM jugadores j5
+			WHERE j5.Categoria = 85
+			GROUP BY j5.Id_Club
+		) jJoven
+	WHERE jViejo.Id_Club = c.Id_Club AND jJoven.Id_Club = c.Id_Club
+GO
 
-SELECT datediff(dd, jViejo.Fecha_Nac, jJoven.Fecha_Nac) AS diferencia
-FROM Clubes c, 
-	(
-		SELECT TOP 
-		FROM Jugadores j4
-		WHERE j4.Categoria = 84
-			AND j4.Id_Club = c.Id_Club
-		ORDER BY j4.Fecha_Nac ASC
-	) jViejo,
-	(
-		SELECT * 
-		FROM jugadorores j5
-		WHERE j4.Categoria = 85
-			AND j4.Id_Club = c.Id_Club
-		ORDER BY j4.Fecha_Nac DESC
-	) jJoven,
+SELECT * FROM vw1037546_23052017
 
