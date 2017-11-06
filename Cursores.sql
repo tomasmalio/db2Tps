7e6f6061c76292a70f7f9fe46f01df75f3c22826
martinpagliaro <31446228+martinpagliaro@users.noreply.github.com>
2017-11-06 17:28:21 -0300

    Create Cursores.sql

---
+++ Cursores.sql
@@ -0,0 +1,50 @@
+/*CURSORES*/
+
+/*6.1. Definir un Cursor:
+Que liste la ficha de los pacientes de los últimos seis meses conforme al siguiente formato de salida:
+Datos del paciente.
+Identificación del médico.
+Detalle de los estudios realizados.*/
+
+
+/*6.2. Definir un Cursor:
+Que liste el detalle de los planes que cubren un determinado estudio identificando el porcentaje cubierto y la obra social, según formato:
+Estudio.
+Obra social.
+Plan y Cobertura (ordenado en forma decreciente).*/
+
+
+/*6.3. Crear una StoredProcedure que defina un Cursor:
+Que liste el resumen mensual de los importes a cargo de una obra social.
+INPUT: nombre de la obra social, mes y año a liquidar.
+Obra social
+Nombre del Instituto
+Detalle del estudio
+Subtotal del Instituto
+Total de la obra social*/
+
+
+/*6.4. Crear una Stored Procedure que defina un Cursor:
+Que devuelva una tabla de referencias cruzadas que represente el importe mensual abonado a cada instituto en los últimos n meses.
+INPUT: entero que representa los n meses anterioes.
+Mes año Mes año Mes año Mes año Total Inst.
+Inst. A - - $ $
+Inst. B $ $ - $
+Total $ $ $ $*/
+
+
+/*6.5. Definir un Cursor:
+Que actualiceel campo observaciones de la tabla historias con las siguientes indicaciones:
+Repetir estudio: si el mismo se realizó en el segundo instituto registrado en la tabla (orden alfabético).
+Diagnóstico no confirmado: si el mismo se realizó en cualquier otro instituto y fue solicitado por el
+tercer médico de la tabla (orden alfabético).*/
+
+
+/*6.6. Definir un Cursor:
+Que actualiceel campo precio de la tabla precios incrementando en un 2% los mismos para cada estudio de distinta especialidad a las restantes.
+Ej.: 1º especialidad un 2%, 2º especialidad un 4%, ... */
+
+/*6.7. Definir un Cursor:
+Que elimine todas las claves foráneas de la tabla Registro (registro de los estudios realizados).
+Debe proyectar el nombre de las restricciones realizadas indicando la tabla referenciada.
+- Volver a crearlas con un proceso externo al cursor - */
