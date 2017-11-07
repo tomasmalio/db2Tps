866fadb7c63a3c12d3993140607d885cf7e4b46d
Martin <uhaldejm@gmail.com>
2017-11-07 20:28:43 -0300

    Update cursores.sql

--- unidad-6/cursores.sql
+++ unidad-6/cursores.sql
@@ -19,3 +19,37 @@ WHILE @@FETCH_STATUS = 0
    END
 CLOSE lista_pacientes
 DEALLOCATE lista_pacientes
+
+
+
+--6.2. Definir un Cursor:
+--Que liste el detalle de los planes que cubren un determinado estudio identificando el porcentaje cubierto y la obra social, según formato:
+--Estudio.
+--Obra social.
+--Plan y Cobertura (ordenado en forma decreciente).
+--select * from Planes p join Plan_Estudio pe on p.id = pe.id_plan join Estudio e on e.id = pe.id_estudio
+
+   DECLARE @nombre_estudio varchar(50), @id_os smallint, @id_plan smallint, @cobertura decimal(3,0)
+
+   DECLARE Cursor_planes CURSOR FORWARD_ONLY READ_ONLY
+   FOR select e.nombre_estudio, p.id_obra_social, p.id,pe.cobertura from Planes p join Plan_Estudio pe on p.id = pe.id_plan join Estudio e on e.id = pe.id_estudio
+   OPEN Cursor_planes
+   FETCH NEXT FROM Cursor_planes INTO @nombre_estudio, @id_os, @id_plan, @cobertura 
+   
+   IF @@FETCH_STATUS <> 0 
+        PRINT 'NO TIENE planes que cubren un determinado estudio'
+   ELSE
+        PRINT '==  TIENE planes que cubren un determinado estudio =='     
+	
+	DECLARE @mensaje varchar(255)
+   WHILE @@FETCH_STATUS = 0
+   BEGIN
+	print @nombre_estudio + convert(varchar(25),@id_os) +','+  convert(varchar(25),@id_plan) +','+  convert(varchar(3),@cobertura) 
+     
+      FETCH NEXT FROM Cursor_planes INTO @nombre_estudio, @id_os, @id_plan, @cobertura 
+   END
+
+   CLOSE Cursor_planes
+   DEALLOCATE Cursor_planes
+   GO
+
