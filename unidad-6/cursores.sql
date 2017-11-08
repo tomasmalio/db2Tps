cb4570b21ce050f638012f1ffba6738fe395a444
Martin <uhaldejm@gmail.com>
2017-11-07 21:42:39 -0300

    Update cursores.sql

--- unidad-6/cursores.sql
+++ unidad-6/cursores.sql
@@ -53,3 +53,55 @@ DEALLOCATE lista_pacientes
    DEALLOCATE Cursor_planes
    GO
 
+
+-- 6.3.  Crear una StoredProcedure que defina un Cursor:
+--Que liste el resumen mensual de los importes a cargo de una obra social.
+--INPUT: nombre de la obra social, mes y año a liquidar.
+--Obra social
+--Nombre del Instituto
+--Detalle del estudio
+--Subtotal del Instituto
+--Total de la obra social
+
+create procedure sp_cursor_resumen_mensual @nombre_obra_social varchar(50),@mes int,@anio int
+as
+
+ DECLARE @nombre_instituto varchar(50),@detalle_estudio varchar(50), @nombre_os varchar(50), @subtotalinstituo decimal(10), @totalOS decimal(10) = 0
+
+   DECLARE Cursor_resumen CURSOR FORWARD_ONLY READ_ONLY
+   FOR select i.nombre_instituto ,e.nombre_estudio,o.nombre,sum(precio)as[subtotal x instituto] from ObraSocial o join Planes p on p.id_obra_social=o.id
+join Plan_Estudio pe on pe.id_plan = p.id
+join Instituto_Estudio ie on ie.id_estudio = pe.id_estudio
+join Estudio e on ie.id_estudio= e.id join Instituto i on i.id = ie.id_instituto
+join Registro r on r.id_estudio = e.id
+where o.nombre=@nombre_obra_social and datepart(mm, r.fecha_estudio) = @mes and datepart(YY, fecha_estudio) = @anio group by i.nombre_instituto,o.nombre,e.nombre_estudio
+   OPEN Cursor_resumen
+   FETCH NEXT FROM Cursor_resumen INTO @nombre_instituto, @detalle_estudio, @nombre_os, @subtotalinstituo
+   
+   IF @@FETCH_STATUS <> 0 
+        PRINT 'NO TIENE planes que cubren un determinado estudio'
+   ELSE
+        PRINT '==  TIENE planes que cubren un determinado estudio =='     
+	
+	DECLARE @mensaje varchar(255)
+   WHILE @@FETCH_STATUS = 0
+   BEGIN
+  set @totalOS = convert(varchar(50),@subtotalinstituo) + @totalOS
+		PRINT @nombre_instituto
+		PRINT @detalle_estudio
+		PRINT @nombre_os
+		PRINT convert(varchar(50),@subtotalinstituo)
+		
+	
+     FETCH NEXT FROM Cursor_resumen INTO @nombre_instituto, @detalle_estudio, @nombre_os, @subtotalinstituo
+   END
+   PRINT 'El total de la OS fue :'+convert(varchar(50),@totalOS)
+   CLOSE Cursor_resumen
+   DEALLOCATE Cursor_resumen
+   GO
+
+   EXEC sp_cursor_resumen_mensual 'OSDE',5,2017
+
+
+
+
