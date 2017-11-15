e56fc8402789bddb78c147592d46e82825391e01
martinpagliaro <31446228+martinpagliaro@users.noreply.github.com>
2017-11-15 17:09:09 -0300

    Update Transacciones.sql

--- Transacciones.sql
+++ Transacciones.sql
@@ -8,7 +8,7 @@ Se debe actualizar en cadena todas las tablas afectadas al proceso.
 En caso de error se anulará la transacción presentando el mensaje correspondiente y devolviendo un código de error.
 Modificar en caso de ser necesaria la definición de los atributos de las tablas que impidan la ejecución de la transacción.*/
 
-CREATE PROCEDURE trans_modificar_obrasocial
+CREATE PROCEDURE trans_obrasocial
 @siglaNueva sigla, 
 @siglaAnterior sigla, 
 @nombreNuevo varchar(80),
@@ -40,6 +40,158 @@ Crear las tablas ex_pacientes y ex_historias (si no existen) y grabar los datos 
 la registración del usuario que invocó la transacción y la fecha.
 Se deben eliminar en cadena todas las tablas afectadas al proceso, en caso de error se anulará la transacción presentando el 
 mensaje correspondiente y devolviendo un código de error.*/
+
+CREATE PROCEDURE sp_paciente
+@dniPac dni
+AS
+BEGIN
+	BEGIN transaction
+		IF(NOT EXISTS (SELECT * FROM sys.tables as tabla
+			WHERE tabla.name = 'ex_pacientes'))
+			BEGIN
+				CREATE TABLE ex_pacientes (
+					exPaciente_dni dni not null,
+					exPaciente_nombre varchar(80),
+					exPaciente_apellido varchar(80),
+					usuario varchar(80),
+					fecha date
+				)
+			END
+		if(@@error <> 0)
+			BEGIN
+				rollback tran
+			END
+		ELSE
+			BEGIN
+				commit
+			END
+	
+	SELECT * FROM Registro
+
+	BEGIN transaction
+		IF(NOT EXISTS (SELECT * FROM sys.tables tabla
+			WHERE tab.name = 'ex_registros'))
+			BEGIN
+				CREATE TABLE ex_registros (
+					exFecha date,
+					exIdEstudio id,
+					exIdInstituto id,
+					ex_matricula_medico id,
+					exIdObraSocial id,
+					exIdPaciente id,
+					exPago float,
+					exResultado varchar(50),
+					exAbonado bit
+				)
+			END
+		IF (@@error <> 0)
+			begin
+				rollback tran
+			END
+		ELSE
+			begin
+				commit
+			END
+			
+	
+	declare cr_paciente_to_delete cursor scroll
+	for
+		select pa.dni, pa.nombre, pa.apellido from Paciente as pa
+		where pa.dni = @dniPac
+	
+	declare @dni dni, @nombre varchar(20), @apellido varchar(20)
+	
+	open cr_paciente_to_delete
+	
+	fetch next from cr_paciente_to_delete
+	into @dni, @nombre, @apellido
+	while @@fetch_status = 0
+		begin
+			insert into ex_pacientes values (@dni, @nombre, @apellido, CURRENT_USER, cast(getdate() as varchar))
+			fetch next from cr_paciente_to_delete
+			into @dni, @nombre, @apellido
+		end
+	close cr_paciente_to_delete
+	deallocate cr_paciente_to_delete
+	
+	declare @idPacienteABorrar id
+
+	select @idPacienteABorrar = pa.idPaciente
+	from Paciente as pa
+	where pa.dni = @dni
+
+	delete Paciente_PlanB
+	where idPaciente = @idPacienteABorrar
+
+	delete Paciente
+	where idPaciente = @idPacienteABorrar
+	
+	declare cr_historial_to_delete cursor scroll
+	for
+		select re.fecha, re.idEstudio, re.idInstituto, re.idMedico, re.idObraSocial, re.idPaciente, re.idRegistro, re.pago, re.resultado from Registro as re
+		where re.idPaciente = @idPacienteABorrar
+	
+	declare @fecha date, @idEstudio id, @idInstituto id, @idMedico id, @idObraSocial id, @idPaciente id, @pago float, @resultado varchar(50), @abonado bit
+
+	open cr_historial_to_delete
+	
+	fetch next from cr_historial_to_delete
+	into @fecha, @idEstudio, @idInstituto, @idMedico, @idObraSocial, @idPaciente, @pago, @resultado, @abonado
+	while(@@fetch_status = 0)
+		begin
+			insert into ex_registros values (@fecha, @idEstudio, @idInstituto, @idMedico, @idObraSocial, @idPaciente, @pago, @resultado, @abonado)
+			fetch next from cr_historial_to_delete
+			into @fecha, @idEstudio, @idInstituto, @idMedico, @idObraSocial, @idPaciente, @pago, @resultado, @abonado
+		end
+
+	close cr_historial_to_delete
+	deallocate cr_historial_to_delete
+	
+	delete Registro
+	where Registro.idPaciente = @idPacienteABorrar
+end
+
+
+create procedure sp_eliminar_paciente
+@dniPaciente dni
+as
+begin
+	begin transaction
+		ALTER TABLE registro DISABLE TRIGGER historias_pacientes
+
+		if(@@error <> 0)
+			begin
+				RAISERROR ('Error en deshabilitar el trigger', 16, 1)
+				ROLLBACK TRAN
+				return
+			end
+	
+		begin transaction
+			exec sp_delete_paciente @dniPaciente
+		
+			if(@@error <> 0)
+				begin
+					RAISERROR ('Error en delete de paciente', 16, 1)
+					ROLLBACK TRAN
+					return
+				end
+		
+			begin transaction
+				ALTER TABLE registro ENABLE TRIGGER historias_pacientes
+			
+				if(@@error <> 0)
+					begin
+						RAISERROR ('Error en activar el trigger', 16, 1)
+						ROLLBACK TRAN
+						return
+					end
+			
+	while(@@TRANCOUNT <> 0)
+		begin
+			COMMIT
+		end
+end
+
 
 
 /*7.3. Definir una transacción que elimine lógicamente de la Base de Datos a todos los médicos de una determinada especialidad.
