6f2e8c29a1c3c3f5125af336dd50f15cd265afd6
tomasmalio <tomasmalio@gmail.com>
2017-11-28 21:56:08 -0300

    Vistas de Sistema

--- unidad-5/vistas-del-sistema.sql
+++ unidad-5/vistas-del-sistema.sql
@@ -1,4 +1,20 @@
 /**
 	5.1. Listar los usuarios de la base de datos.
 **/
-SELECT * FROM sysusers
+use master
+
+SELECT * FROM sysusers --Para SQL2000
+
+SELECT * FROM syslogins --Para SQL2000
+
+SELECT * FROM sys.database_principals 
+
+SELECT * FROM sys.server_principals
+
+SELECT * FROM sys.sql_logins
+
+/**
+	5.2. Listar los tipos de datos creados.
+**/
+select name from systypes;
+
