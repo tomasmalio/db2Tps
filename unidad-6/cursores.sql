f4a1e8b4cd3910af9e80eed4940f52172e3c86f6
martinpagliaro <31446228+martinpagliaro@users.noreply.github.com>
2017-11-28 22:08:15 -0300

    Update cursores.sql

--- unidad-6/cursores.sql
+++ unidad-6/cursores.sql
@@ -101,6 +101,40 @@ where o.nombre=@nombre_obra_social and datepart(mm, r.fecha_estudio) = @mes and 
    GO
 
    EXEC sp_cursor_resumen_mensual 'OSDE',5,2017
+   
+/*
+6.4. Crear una Stored Procedure que defina un Cursor:
+Que devuelva una tabla de referencias cruzadas que represente el importe mensual abonado a
+cada instituto en los últimos n meses.
+INPUT: entero que representa los n meses anterioes.
+Mes año Mes año Mes año Mes año Total Inst.
+Inst. A - - $ $
+Inst. B $ $ - $
+Total $ $ $ $
+*/
+
+/*
+6.5. Definir un Cursor:
+Que actualiceel campo observaciones de la tabla historias con las siguientes indicaciones:
+Repetir estudio: si el mismo se realizó en el segundo instituto registrado en la tabla (orden
+alfabético).
+Diagnóstico no confirmado: si el mismo se realizó en cualquier otro instituto y fue
+solicitado por el tercer médico de la tabla (orden alfabético).
+*/
+
+/*
+6.6. Definir un Cursor:
+Que actualiceel campo precio de la tabla precios incrementando en un 2% los mismos para
+cada estudio de distinta especialidad a las restantes.
+Ej.: 1º especialidad un 2%, 2º especialidad un 4%, ...
+*/
+
+/*
+6.7. Definir un Cursor:
+Que elimine todas las claves foráneas de la tabla Registro (registro de los estudios realizados).
+Debe proyectar el nombre de las restricciones realizadas indicando la tabla referenciada.
+- Volver a crearlas con un proceso externo al cursor -
+*/
 
 
 
