9e660cd90e53f341808e840cd2355f76944be76f
tomasmalio <tomasmalio@gmail.com>
2017-12-17 22:10:02 -0300

    Final

--- ejercicios-de-final/final-tema-2.sql
+++ ejercicios-de-final/final-tema-2.sql
@@ -137,165 +137,157 @@ AS
 
 	BEGIN TRANSACTION transaction_02
 	
-		-- Establecer en cada categoría de cada club hasta un máximo de 11 jugadores como titulares, 
-		-- los restantes como suplentes (se debe invocar la función del punto 2)
-		DECLARE listado_de_equipos CURSOR FOR
-			SELECT j.Id_Club, j.Categoria 
-			FROM Jugadores j 
-			GROUP BY j.Id_Club, j.Categoria  
-			ORDER BY j.Id_Club, j.Categoria ASC
+	-- Establecer en cada categoría de cada club hasta un máximo de 11 jugadores como titulares, 
+	-- los restantes como suplentes (se debe invocar la función del punto 2)
+	DECLARE listado_de_equipos CURSOR FOR
+		SELECT j.Id_Club, j.Categoria 
+		FROM Jugadores j 
+		GROUP BY j.Id_Club, j.Categoria  
+		ORDER BY j.Id_Club, j.Categoria ASC
 
-		OPEN listado_de_equipos
+	OPEN listado_de_equipos
 
-		FETCH NEXT FROM listado_de_equipos INTO @Id_Club_buscado, @Categoria_buscado
+	FETCH NEXT FROM listado_de_equipos INTO @Id_Club_buscado, @Categoria_buscado
 
-		-- Recorremos cada equipo para saber si tiene 11 jugadores como titulares
-		WHILE @@FETCH_STATUS = 0
-			BEGIN 
-				-- Obtenemos la cantidad de jugadores que tiene el equipo de esa categoria
-				SET @cant_jugadores_por_equipo_cat = (
-															SELECT COUNT(*)
-															FROM Jugadores j
-															WHERE 
-																j.Id_Club = @Id_Club_buscado
-																AND j.Categoria = @Categoria_buscado
-														)
+	-- Recorremos cada equipo para saber si tiene 11 jugadores como titulares
+	WHILE @@FETCH_STATUS = 0
+		BEGIN 
+			-- Obtenemos la cantidad de jugadores que tiene el equipo de esa categoria
+			SET @cant_jugadores_por_equipo_cat = (
+														SELECT COUNT(*)
+														FROM Jugadores j
+														WHERE 
+															j.Id_Club = @Id_Club_buscado
+															AND j.Categoria = @Categoria_buscado
+													)
 
-				-- Buscamos todos los jugadores que tiene el equipo de esa categoria
-				-- utilizando la función del punto 2 (fn1037546_jovenes)
-				DECLARE jugadores_a_titular_suplentes CURSOR FOR
-					SELECT Tipodoc, Nrodoc 
-					FROM fn1037546_jovenes (@Id_Club_buscado, @Categoria_buscado, @cant_jugadores_por_equipo_cat)
+			-- Buscamos todos los jugadores que tiene el equipo de esa categoria
+			-- utilizando la función del punto 2 (fn1037546_jovenes)
+			DECLARE jugadores_a_titular_suplentes CURSOR FOR
+				SELECT Tipodoc, Nrodoc 
+				FROM fn1037546_jovenes (@Id_Club_buscado, @Categoria_buscado, @cant_jugadores_por_equipo_cat)
 
-				OPEN jugadores_a_titular_suplentes
+			OPEN jugadores_a_titular_suplentes
 
-				FETCH NEXT FROM jugadores_a_titular_suplentes INTO @Tipodoc_jugador_buscado, @Nrodoc_jugador_buscado
-				
-				-- Generamos una variable para recorrer 
-				SET @cantidad_jugadores_ingresados = 1
+			FETCH NEXT FROM jugadores_a_titular_suplentes INTO @Tipodoc_jugador_buscado, @Nrodoc_jugador_buscado
+			
+			-- Generamos una variable para recorrer 
+			SET @cantidad_jugadores_ingresados = 1
 
-				-- Recorremos el cursor generado anteriormente con el objetivo de poner
-				-- 11 jugadores en titulares y el resto en suplentes
-				WHILE @@FETCH_STATUS = 0
-					BEGIN
-						-- Como todavía no se ingresaron más de 11 jugadores es titular
-						IF (@cantidad_jugadores_ingresados <= 11)
-							BEGIN
-								INSERT INTO Titulares (Tipodoc, Nrodoc) VALUES (@Tipodoc_jugador_buscado, @Nrodoc_jugador_buscado)
-							END
-						-- Cuando superan los 11 jugadores titulares se los ingresa en Suplentes
-						ELSE 
-							BEGIN
-								INSERT INTO Suplentes (Tipodoc, Nrodoc) VALUES (@Tipodoc_jugador_buscado, @Nrodoc_jugador_buscado)
-							END
+			-- Recorremos el cursor generado anteriormente con el objetivo de poner
+			-- 11 jugadores en titulares y el resto en suplentes
+			WHILE @@FETCH_STATUS = 0
+				BEGIN
+					-- Como todavía no se ingresaron más de 11 jugadores es titular
+					IF (@cantidad_jugadores_ingresados <= 11)
+						BEGIN
+							INSERT INTO Titulares (Tipodoc, Nrodoc) VALUES (@Tipodoc_jugador_buscado, @Nrodoc_jugador_buscado)
+						END
+					-- Cuando superan los 11 jugadores titulares se los ingresa en Suplentes
+					ELSE 
+						BEGIN
+							INSERT INTO Suplentes (Tipodoc, Nrodoc) VALUES (@Tipodoc_jugador_buscado, @Nrodoc_jugador_buscado)
+						END
 
-						-- Incrementamos una variable para saber cuantos jugadores se ingresaron
-						SET @cantidad_jugadores_ingresados = @cantidad_jugadores_ingresados + 1
+					-- Incrementamos una variable para saber cuantos jugadores se ingresaron
+					SET @cantidad_jugadores_ingresados = @cantidad_jugadores_ingresados + 1
 
-						FETCH NEXT FROM jugadores_a_titular_suplentes INTO @Tipodoc_jugador_buscado, @Nrodoc_jugador_buscado
-					END
+					FETCH NEXT FROM jugadores_a_titular_suplentes INTO @Tipodoc_jugador_buscado, @Nrodoc_jugador_buscado
+				END
 
-				CLOSE jugadores_a_titular_suplentes
-				DEALLOCATE jugadores_a_titular_suplentes
+			CLOSE jugadores_a_titular_suplentes
+			DEALLOCATE jugadores_a_titular_suplentes
 
-				
-				FETCH NEXT FROM listado_de_equipos INTO @Id_Club_buscado, @Categoria_buscado
-			END  
+			
+			FETCH NEXT FROM listado_de_equipos INTO @Id_Club_buscado, @Categoria_buscado
+		END  
 
-		CLOSE listado_de_equipos
-		DEALLOCATE listado_de_equipos
+	CLOSE listado_de_equipos
+	DEALLOCATE listado_de_equipos
 
-		-- Establecemos un punto de grabación
-		SAVE TRANSACTION punto_de_grabacion
+	-- Establecemos un punto de grabación
+	SAVE TRANSACTION punto_de_grabacion
 
-	--COMMIT TRANSACTION st_01
 	-- Eof primera transacción
 
 	-- Incorporar como titular para cada categoría de cada club al jugador suplente (de ese club
 	-- y categoría) que posea el nombre más largo en cantidad de caracteres.
-	--BEGIN TRANSACTION st_02
 
-		DECLARE listado_de_equipos CURSOR FOR
-			SELECT j.Id_Club, j.Categoria 
-			FROM Jugadores j 
-			GROUP BY j.Id_Club , j.Categoria 
-			ORDER BY j.Id_Club, j.Categoria ASC
+	DECLARE listado_de_equipos CURSOR FOR
+		SELECT j.Id_Club, j.Categoria 
+		FROM Jugadores j 
+		GROUP BY j.Id_Club , j.Categoria 
+		ORDER BY j.Id_Club, j.Categoria ASC
 
-		OPEN listado_de_equipos
+	OPEN listado_de_equipos
 
-		FETCH NEXT FROM listado_de_equipos INTO @Id_Club_buscado, @Categoria_buscado
+	FETCH NEXT FROM listado_de_equipos INTO @Id_Club_buscado, @Categoria_buscado
 
-		-- Recorremos cada equipo para buscar el que tiene más nombre del equipo Suplente
-		-- para pasarlo al equipo Titular
-		WHILE @@FETCH_STATUS = 0
-			BEGIN 
-				-- Generamos un cursor con el listado de jugadores ordenados por nombre
-				-- más largo de un Club y Categoria (id_club / categoria) que sea Suplente
-				DECLARE jugadores_sup_por_nombre_largo CURSOR SCROLL FOR
-					SELECT j.Tipodoc, j.Nrodoc
-					FROM Jugadores j
-					WHERE 
-						j.Id_Club = @Id_Club_buscado
-						AND j.Categoria = @Categoria_buscado
-						AND j.Nrodoc IN (
-											SELECT s.Nrodoc 
-											FROM Suplentes s
-										)
-					ORDER BY len(j.Nombre) ASC
+	-- Recorremos cada equipo para buscar el que tiene más nombre del equipo Suplente
+	-- para pasarlo al equipo Titular
+	WHILE @@FETCH_STATUS = 0
+		BEGIN 
+			-- Generamos un cursor con el listado de jugadores ordenados por nombre
+			-- más largo de un Club y Categoria (id_club / categoria) que sea Suplente
+			DECLARE jugadores_sup_por_nombre_largo CURSOR SCROLL FOR
+				SELECT j.Tipodoc, j.Nrodoc
+				FROM Jugadores j
+				WHERE 
+					j.Id_Club = @Id_Club_buscado
+					AND j.Categoria = @Categoria_buscado
+					AND j.Nrodoc IN (
+										SELECT s.Nrodoc 
+										FROM Suplentes s
+									)
+				ORDER BY len(j.Nombre) ASC
 
-				OPEN jugadores_sup_por_nombre_largo
+			OPEN jugadores_sup_por_nombre_largo
 
-				-- Selecccionamos únicamente el primer jugador que cumpla con todas las condiciones
-				FETCH FIRST FROM jugadores_sup_por_nombre_largo INTO @Tipodoc_jugador_buscado, @Nrodoc_jugador_buscado
+			-- Selecccionamos únicamente el primer jugador que cumpla con todas las condiciones
+			FETCH FIRST FROM jugadores_sup_por_nombre_largo INTO @Tipodoc_jugador_buscado, @Nrodoc_jugador_buscado
 
-				IF ((SELECT t.Nrodoc FROM Titulares t WHERE t.Tipodoc = @Tipodoc_jugador_buscado AND t.Nrodoc = @Nrodoc_jugador_buscado) <> @Nrodoc_jugador_buscado)
-					BEGIN
-						-- Insertamos en Titulares el jugador con el nombre más largo
-						INSERT INTO Titulares (Tipodoc, Nrodoc) VALUES (@Tipodoc_jugador_buscado, @Nrodoc_jugador_buscado)
+			IF ((SELECT t.Nrodoc FROM Titulares t WHERE t.Tipodoc = @Tipodoc_jugador_buscado AND t.Nrodoc = @Nrodoc_jugador_buscado) <> @Nrodoc_jugador_buscado)
+				BEGIN
+					-- Insertamos en Titulares el jugador con el nombre más largo
+					INSERT INTO Titulares (Tipodoc, Nrodoc) VALUES (@Tipodoc_jugador_buscado, @Nrodoc_jugador_buscado)
 
-						-- Borramos de Suplentes el jugador con el nombre más largo
-						DELETE Suplentes WHERE Tipodoc = @Tipodoc_jugador_buscado AND Nrodoc = @Nrodoc_jugador_buscado
-					END
-				ELSE
-					BEGIN
-						PRINT 'El jugador del equipo ' + convert(varchar(5), @Id_Club_buscado) + ' cuya categoria es ' + convert(varchar(5), @Categoria_buscado) + ' ya se encontraba en los Titulares'
-					END				
+					-- Borramos de Suplentes el jugador con el nombre más largo
+					DELETE Suplentes WHERE Tipodoc = @Tipodoc_jugador_buscado AND Nrodoc = @Nrodoc_jugador_buscado
+				END
+			ELSE
+				BEGIN
+					PRINT 'El jugador del equipo ' + convert(varchar(5), @Id_Club_buscado) + ' cuya categoria es ' + convert(varchar(5), @Categoria_buscado) + ' ya se encontraba en los Titulares'
+				END				
 
-				CLOSE jugadores_sup_por_nombre_largo
-				DEALLOCATE jugadores_sup_por_nombre_largo
+			CLOSE jugadores_sup_por_nombre_largo
+			DEALLOCATE jugadores_sup_por_nombre_largo
 
-				FETCH NEXT FROM listado_de_equipos INTO @Id_Club_buscado, @Categoria_buscado
-			END
+			FETCH NEXT FROM listado_de_equipos INTO @Id_Club_buscado, @Categoria_buscado
+		END
 
-		CLOSE listado_de_equipos
-		DEALLOCATE listado_de_equipos
+	CLOSE listado_de_equipos
+	DEALLOCATE listado_de_equipos
 
-	--COMMIT TRANSACTION st_02
 	-- Eof segunda transacción
 
 	-- Proyectar todos los jugadores suplentes
-	--BEGIN TRANSACTION st_03
-		PRINT 'Proyectamos todos los jugadores suplentes'
+	PRINT 'Proyectamos todos los jugadores suplentes'
 
-		SELECT j.Tipodoc, j.Nrodoc, j.Nombre, j.Fecha_Nac, j.Categoria, j.Id_Club, c.Nombre
-		FROM Jugadores j
-		LEFT JOIN Clubes c ON (c.Id_Club = j.Id_Club)
-		WHERE j.Nrodoc IN (
-							SELECT s.Nrodoc 
-							FROM Suplentes s 
-							WHERE 
-								s.Tipodoc = j.Tipodoc 
-								AND s.Nrodoc = j.Nrodoc
-						)
-
-	--COMMIT TRANSACTION st_03
+	SELECT j.Tipodoc, j.Nrodoc, j.Nombre, j.Fecha_Nac, j.Categoria, j.Id_Club, c.Nombre
+	FROM Jugadores j
+	LEFT JOIN Clubes c ON (c.Id_Club = j.Id_Club)
+	WHERE j.Nrodoc IN (
+						SELECT s.Nrodoc 
+						FROM Suplentes s 
+						WHERE 
+							s.Tipodoc = j.Tipodoc 
+							AND s.Nrodoc = j.Nrodoc
+					)
 	-- Eof tercera transacción
 
-	--BEGIN TRANSACTION st_04
-		-- Deshacemos la transacción hasta el punto de grabación 
-		ROLLBACK TRANSACTION punto_de_grabacion
-
-	--COMMIT TRANSACTION st04
+	-- Deshacemos la transacción hasta el punto de grabación 
+	ROLLBACK TRANSACTION punto_de_grabacion
+	-- Eof cuarta transacción
 
 	COMMIT TRANSACTION transaction_02
 GO
