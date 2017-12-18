9a8fc331c2fa6dfb66619f87a326f05c3e062c6c
martinpagliaro <31446228+martinpagliaro@users.noreply.github.com>
2017-12-17 23:05:39 -0300

    Create Final Regular 2016-2017.sql

---
+++ ejercicios-de-final/Final Regular 2016-2017.sql
@@ -0,0 +1,15 @@
+/*1 - Resolver mediante la utilizacion de cursores
+Generar un listado con los datos existentes de los partidos de la base de jugadores, conforme a las siguientes caracteristicas:
+a - Debe corresponder a los partidos de referentes a la fecha (nº de fecha), en la que se convirtieron la mayor cantidad de goles.
+b - Indicar la cantidad de goles convertidos por cada club en esa fecha, discriminados por categoria.
+c - La nomina de clubes que convirtieron goles debe ordenarse de mayor a menor por cantidad total de goles entre ambas categorias.
+d - Los resultados a mostrar debe respetar el siguiente formato:
+Fecha con mayor cantidad de goles convertidos.
+  Nº fecha:(nº)           Cantidad de goles: (total de la fecha) 
+  
+  Goles convertidos por los clubes ordenados de mayor a menor.
+  Club:(nombre)           Total de goles:(total del club)
+        Categoria 84: (subtotal de la categoria)
+        Categoria 85: (subtotal de la categoria)
+*/
+
