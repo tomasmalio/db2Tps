831bc37670c532886fd3fcd37c00ee5ba63d73c8
martinpagliaro <31446228+martinpagliaro@users.noreply.github.com>
2017-12-17 23:23:25 -0300

    Update Final Regular 2016-2017.sql

--- ejercicios-de-final/Final Regular 2016-2017.sql
+++ ejercicios-de-final/Final Regular 2016-2017.sql
@@ -1,4 +1,5 @@
-/*1 - Resolver mediante la utilizacion de cursores
+/*
+1 - Resolver mediante la utilizacion de cursores
 Generar un listado con los datos existentes de los partidos de la base de jugadores, conforme a las siguientes caracteristicas:
 a - Debe corresponder a los partidos de referentes a la fecha (nº de fecha), en la que se convirtieron la mayor cantidad de goles.
 b - Indicar la cantidad de goles convertidos por cada club en esa fecha, discriminados por categoria.
@@ -11,5 +12,32 @@ Fecha con mayor cantidad de goles convertidos.
   Club:(nombre)           Total de goles:(total del club)
         Categoria 84: (subtotal de la categoria)
         Categoria 85: (subtotal de la categoria)
+*/
+
+/*
+2 - Implementar una unica transaccion que invoque una funcion y actualice y proyecte la tabla de partidos, conforme a siguiente detalle:
+a - Definir una función escalar que retorne en nº de fecha donde se registraron la menor cantidad de empates entre los clubes de la zona 2.
+Debe resolverse por correlacionada.
+b - Iniciar una transaccion que actualice todos los partidos de la zona 2 correspondiente a la fecha retornada por la funcion, agregando un
+gol a cada club visitante en la categoria 85.
+c - Modificar la tabla poscate285 en funcion de los goles agregados en el punto b. Debiendo:
+  I - Actualizar la cantidad de partidos ganados, empatados y perdidos de los clubes afectados.
+  II - Actualizar la cantidad de goles (a favor) y golesC (en contra) de los mismos.
+  III - Actualizar los puntos de dichos clubes teniendo en cuenta 3 puntos por partido ganado y 1 punto por partido empatado.
+d - Proyectar la tabla poscate285 luego de ser actualizada.
+e - Deshacer el punto c y confirmar el punto b de la transaccion antes de finalizarla.
+
+Debe controlar las salidas por error mostrando un mensaje identificatorio del error. 
+No utilizar cursores.
+*/
+
+/*
+3 - Definir un desencadenador que se accione cuando se modifica el club al que pertenecen los jugadores.
+a - La modificacion del club se interpreta como un intercambio entre clubes, de modo que si los jugadores del club1 se modifican al 
+club2, los jugadores del club2 pasan al club1.
+b - La modificacion se realiza intercambiando la misma cantidad de jugadores entre los dos clubes de la misma zona y en la misma 
+categoria.
+c - Si la cantidad a intercambiar no es posible satisfacerla, se ajusta a la cantidad correcta para realizar el intercambio.
+No utilizar cursores.
 */
 
